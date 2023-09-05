5b3ca17487f6ed137343439159ce06dea5e5f200
Ryan Cheng <rz.cheng06@gmail.com>
2023-09-04 20:56:33 -0700

    added closeAccount method

--- ATM.java
+++ ATM.java
@@ -13,10 +13,24 @@ public class ATM {
 
     public void openAccount(String userId, double amount) throws Exception {
         if (accountMap.containsKey(userId)){
-            throw new Exception("Account already exists.");
+            throw new Exception("Error: Account already exists.");
         }
         else {
             accountMap.put(userId, amount);
         }
     }
+
+    public void closeAccount (String userId) throws Exception{
+
+        if (!accountMap.containsKey(userId))
+            throw new Exception("Error: User ID does not exist.");
+
+        if (accountMap.get(userId) <= 0) {
+            accountMap.remove(userId);
+        } else {
+            throw new Exception("Error: Must withdraw money before closing.");
+        }
+    }
+
+    
 }
